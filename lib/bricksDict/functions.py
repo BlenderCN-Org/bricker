23e40854446addd3098d3b4784fde2ddd5c54653
“bblanimation” <cgear13@gmail.com>
2019-03-21 13:06:03 -0500

    bug fix for materials

--- lib/bricksDict/functions.py
+++ lib/bricksDict/functions.py
@@ -197,7 +197,7 @@ def createNewMaterial(model_name, rgba, rgba_vals, sss, sat_mat, specular, rough
     mat = mat or bpy.data.materials.new(name=mat_name)
     # set diffuse and transparency of material
     if mat_is_new:
-        mat.diffuse_color = rgba[:3]
+        mat.diffuse_color = rgba if b280() else rgba[:3]
         if scn.render.engine == "BLENDER_RENDER":
             mat.diffuse_intensity = 1.0
             if a0 < 1.0:
